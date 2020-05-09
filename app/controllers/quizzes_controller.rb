ed5072f909bbf5466b9169ec890709230488a00c
chihirotanaka <senno330@gmail.com>
2020-05-09 17:31:45 +0000

    [add]admin追加

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,29 @@
 class QuizzesController < ApplicationController
+
+# サイトtopページ
+  def top
+  end
+
+# ユーザー制作ページ
   def new
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+# クイズ画面
+  def quiz
+  end
+
+  def result
+  end
+
+  def total
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
 Rails.application.routes.draw do
-  get 'quizzes/new'
-  get 'quizzes/edit'
-  get 'users/edit'
-  devise_for :users
 
-  root 'quizes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ユーザー
+  devise_for :users
+  root 'quizes#top'
+  get 'users/:id/home'=>'users#home'
+  resources :users, only:[:edit, :update, :destroy]
+  resources :quizzes, only:[:create, :edit, :update, :destroy] do
+      resource :favorites, only:[:create, :destroy]
+      resource :unfavorites, only:[:create, :destroy]
+  end
+  get 'quizzes/:id/quiz' => 'quizzes#quiz'
+  get 'quizzes/:id/result/:anser=1' => 'quizzes#result'
+  get 'quizzes/:id/total' => 'quizzes#total'
+
+
+  # 管理
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+  	get 'admins/sign_in' => 'admins/sessions#new' , as: :new_admin_session
+    post  'admins/sign_in' => 'admins/sessions#create' ,  as: :admin_session
+    delete 'admins/log_out' => 'admins/sessions#destroy' ,  as: :destroy_admin_session
+  end
+
+  namespace :admins do
+    # get '/top'=>'top#top'
+    resources :movies, only:[:create, :edit, :update]
+  end
 end
